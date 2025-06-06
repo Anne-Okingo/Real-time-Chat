d3149f032a8ee9db6248b9ce90e13a289d1b4fbf
Alice Okingo <annekadiso@gmail.com>
2025-06-06 04:17:31 +0300

    feat: implement an new chat server constructor and also initialized websockets

--- main.go
+++ main.go
@@ -7,5 +7,11 @@ import(
 //ChatServer keeps track of all clients connected to the websocket connection
 type ChatServer struct{
 clients map[*websocket.Conn]bool
+}
 
+//this is a new chat server constructor, it also initializes the websocket
+func NewChatServer() * ChatServer {
+	return &ChatServer {
+		clients: make(map[*websocket.Conn]bool),
+	}
 }
