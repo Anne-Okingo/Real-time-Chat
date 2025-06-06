794dcdbcd4faf36fc82fe091e8d2480b03e6403a
Alice Okingo <annekadiso@gmail.com>
2025-06-06 05:12:50 +0300

    fix server initilization process

--- main.go
+++ main.go
@@ -1,86 +1,87 @@
 package main
 
-import(
-	"golang.org/x/net/websocket"
+import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
+
+	"golang.org/x/net/websocket"
 )
 
-//ChatServer keeps track of all clients connected to the websocket connection
-type ChatServer struct{
-clients map[*websocket.Conn]bool
+// ChatServer keeps track of all clients connected to the websocket connection
+type ChatServer struct {
+	clients map[*websocket.Conn]bool
 }
 
-//this is a new chat server constructor, it also initializes the websocket
+// this is a new chat server constructor, it also initializes the websocket
 func NewChatServer() *ChatServer {
-	return &ChatServer {
+	return &ChatServer{
 		clients: make(map[*websocket.Conn]bool),
 	}
 }
 
-
-//handleConnection function that manages a new websocket connection eg broadcast msgs
-func (cs * ChatServer) handleConnection(ws *websocket.Conn) {
-	//Add new client to the list
+// handleConnection function that manages a new websocket connection eg broadcast msgs
+func (cs *ChatServer) handleConnection(ws *websocket.Conn) {
+	// Add new client to the list
 	clientAddr := ws.RemoteAddr().String()
 	fmt.Printf("Anew client connected:  %s/n", clientAddr)
 	cs.clients[ws] = true
 
-
-	//Ensure clean up after client is disconnected
+	// Ensure clean up after client is disconnected
 	defer func() {
 		ws.Close()
 		delete(cs.clients, ws)
-		fmt.Printf("Client from: %s disconnected \n",clientAddr)
+		fmt.Printf("Client from: %s disconnected \n", clientAddr)
 	}()
 
-	//start listening for maessages from clients
+	// start listening for maessages from clients
 
 	for {
-		 messages := ""
+		messages := ""
 
-		 err := websocket.Message.Receive(ws, &messages)
-		 if err != nil{
-			//client disconnected or error occurred
+		err := websocket.Message.Receive(ws, &messages)
+		if err != nil {
+			// client disconnected or error occurred
 			break
-		 }
+		}
 
-		 fmt.Printf("Received message from %s\n",clientAddr, messages)
+		fmt.Printf("Received message from %s\n", clientAddr, messages)
 
-
-		 //broadcast the message to all clients
-		 cs.broadcast(messages)
+		// broadcast the message to all clients
+		cs.broadcast(messages)
 	}
 }
 
-//broacdcast send the websocket message to all connected clients
-func (cs *ChatServer) broadcast(message string){
-	 for client := range cs.clients{
+// broacdcast send the websocket message to all connected clients
+func (cs *ChatServer) broadcast(message string) {
+	for client := range cs.clients {
 		err := websocket.Message.Send(client, message)
-
-		if err != nil{
-			//if sending fails, assume client disconnected
+		if err != nil {
+			// if sending fails, assume client disconnected
 			fmt.Println("Error sending to client:", err)
 			client.Close()
 			delete(cs.clients, client)
 		}
-	 }
+	}
 }
 
+func main() {
+	http.Handle("/ws", websocket.Handler(func(ws *websocket.Conn) {
+		ws.Config().Origin, _ = url.Parse("http://localhost")
+		fmt.Println("Client connected")
 
+		for {
+			var msg string
+			err := websocket.Message.Receive(ws, &msg)
+			if err != nil {
+				break
+			}
+			fmt.Println("Received:", msg)
+			websocket.Message.Send(ws, "Server: "+msg)
+		}
+	}))
 
-
-func main(){
-	//create a chat server
-
-	server := NewChatServer()
-
-	//setup websocket route
-
-	http.Handle("/chat", websocket.Handler(server.handleConnection))
-
-	//start the web server
-	fmt.Println("Chat server started on :8080, connect at ws://localhost:8080/chat")
+	fmt.Println("Server starting on :8080...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
